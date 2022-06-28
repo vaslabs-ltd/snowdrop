62de55b54d2b3651e20bb05a21008a0c8952752b
George Onisiforou <george.onisiforou@vaslabs.io>
2022-06-28 12:16:08 +0300

    json testing

--- snowdrop-circe/src/main/scala/snowdrop/MaskJson.scala
+++ snowdrop-circe/src/main/scala/snowdrop/MaskJson.scala
@@ -4,6 +4,25 @@ import io.circe.Json
 
 object MaskJson {
   final val StringMask = "*****"
-  def apply(json: Json): Json =
-    Json.fromString(StringMask)
+
+  case class Settings(numberMask: Json, stringMaskingSequence: Json)
+
+  object Settings {
+
+    def default =
+      Settings(Json.fromString(StringMask), Json.fromString(StringMask))
+    
+    def apply(stringMaskingSequence: String): Settings =
+      Settings(Json.fromString(StringMask), Json.fromString(stringMaskingSequence))
+    
+    def apply(intNumberMask: Int): Settings =
+      Settings(Json.fromInt(intNumberMask), Json.fromString(StringMask))
+
+  }
+
+  def apply(json: Json, maskingSettings: Settings = Settings.default): Json =
+    if (json.isNumber)
+      maskingSettings.numberMask
+    else
+      maskingSettings.stringMaskingSequence
 }
--- snowdrop-circe/src/test/scala/snowdrop/MaskLiteralsSpec.scala
+++ snowdrop-circe/src/test/scala/snowdrop/MaskLiteralsSpec.scala
@@ -6,9 +6,50 @@ import org.scalatest.matchers.must.Matchers
 
 class MaskLiteralsSpec extends AnyFlatSpec with Matchers {
 
-  "json masking" must "mask a json string literal" in {
-    MaskJson(Json.fromString("")) mustEqual
-      Json.fromString("*****")
+  "json masking" must "hide a json string" in {
+    val initialJsonString = Json.fromString("hgfdhgfdjhgdsfg")
+    val expectedMaskedJsonString = Json.fromString("*****")
+    MaskJson(initialJsonString) mustEqual
+      expectedMaskedJsonString
   }
+  "json masking" must "be configurable" in{
+    val initialJsonString = Json.fromString("hgfdhgfdjhgdsfg")
+    val expectedMaskedJsonString = Json.fromString("###")
+
+    MaskJson(initialJsonString, MaskJson.Settings(stringMaskingSequence = "###")) mustEqual expectedMaskedJsonString
+  }
+  
+  "json masking" must "hide a json number" in {
+    val initialJsonNumber = Json.fromInt(54569)
+    val expectedMaskedJsonString = Json.fromString("*****")
+    MaskJson(initialJsonNumber) mustEqual
+      expectedMaskedJsonString
+  }
+
+  "json masking" must "be configurable number" in{
+    val initialJsonNumber = Json.fromInt(54569)
+    val expectedMaskedNumber = 123
+    val expectedMaskedJsonNumber = Json.fromInt(expectedMaskedNumber)
+
+    MaskJson(initialJsonNumber, maskingSettings = MaskJson.Settings(expectedMaskedJsonNumber, Json.Null)) mustEqual 
+      expectedMaskedJsonNumber
+  }
+
+  "json masking" must "hide json array fields" in {
+    val item1 = Json.fromString("1")
+    val item2 = Json.fromString("2")
+    val expectedItemMask = Json.fromString("*****")
+
+    MaskJson(Json.arr(item1, item2), maskingSettings = MaskJson.Settings.default) mustEqual 
+      Json.arr(Json.arr(expectedItemMask, expectedItemMask))
+  }
+  
+
+  /*
+  number => Json.fromString("*****") //default behaviour
+  (number, Json) => Json    //configurable
+
+  */
+
 
 }
