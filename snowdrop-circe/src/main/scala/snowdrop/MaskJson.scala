7fd6a02d177e572b727fe4d3a5ea6e3518cf6140
George Onisiforou <george.onisiforou@vaslabs.io>
2022-06-30 15:59:05 +0300

    json array masking

--- snowdrop-circe/src/main/scala/snowdrop/MaskJson.scala
+++ snowdrop-circe/src/main/scala/snowdrop/MaskJson.scala
@@ -1,9 +1,26 @@
 package snowdrop
 
 import io.circe.Json
+import scala.annotation.tailrec
 
 object MaskJson {
   final val StringMask = "*****"
-  def apply(json: Json): Json =
-    Json.fromString(StringMask)
+  def apply(json: Json): Json = {
+    if(json.isArray) 
+      json.mapArray(maskVectorJson)
+    else
+      Json.fromString(StringMask)
+  }
+
+  private def maskVectorJson(vector: Vector[Json]): Vector[Json] = {
+    @tailrec
+    def vectorLoop(unmaskedVector: Vector[Json], maskedVector: Vector[Json]): Vector[Json] = {
+      if(unmaskedVector.isEmpty)
+        maskedVector
+      else
+        vectorLoop(unmaskedVector.tail, maskedVector.appended(apply(unmaskedVector.head)))
+    }
+    vectorLoop(vector, Vector.empty)
+  }
+
 }
--- snowdrop-circe/src/test/scala/snowdrop/MaskLiteralsSpec.scala
+++ snowdrop-circe/src/test/scala/snowdrop/MaskLiteralsSpec.scala
@@ -7,8 +7,27 @@ import org.scalatest.matchers.must.Matchers
 class MaskLiteralsSpec extends AnyFlatSpec with Matchers {
 
   "json masking" must "mask a json string literal" in {
-    MaskJson(Json.fromString("")) mustEqual
+    MaskJson.apply(Json.fromString("")) mustEqual
       Json.fromString("*****")
   }
 
+ "json masking" must "have no effect on an empty json array" in {
+    MaskJson.apply(Json.arr()) mustEqual Json.arr()
+  }
+
+  "json masking" must "mask all elements within an array(string)" in {
+    val input: Json = Json.arr(Json.fromString("fdfdhf"))
+    
+    val expectedOutput: Json = Json.arr(Json.fromString("*****"))
+    
+    MaskJson.apply(input) mustEqual expectedOutput
+  }
+
+  "json masking" must "mask all elements within an array(array(string))" in {
+    val input: Json = Json.arr(Json.arr(Json.fromString("fdfdhf")))
+    
+    val expectedOutput: Json = Json.arr(Json.arr(Json.fromString("*****")))
+    
+    MaskJson.apply(input) mustEqual expectedOutput
+  }
 }
